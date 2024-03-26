ca8c708d5d565fc0510d74692883e9c0196a1b2d
ARK2625123 <arkstar2625123@gmail.com>
2024-03-26 05:55:01 +0500

    Added Intent to Screen 1. On click Listeners for Exit and Sign up Buttons

--- SMD_1/app/src/main/java/com/example/smd_1/MainActivity.java
+++ SMD_1/app/src/main/java/com/example/smd_1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.smd_1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+Button Bsignup ,Bexit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        init();
+        Bsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Screen1.class);
+                startActivity(intent);
+                finish();
+            }
         });
+        Bexit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                System.exit(0);
+            }
+        });
+    }
+
+    public void init()
+    {
+        Bsignup=findViewById(R.id.Bsignup);
+        Bexit=findViewById(R.id.Bexit);
     }
 }
---
+++ SMD_1/app/src/main/java/com/example/smd_1/Screen1.java
@@ -0,0 +1,24 @@
+package com.example.smd_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Screen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.screen_1);
+
+
+
+    }
+
+
+}
