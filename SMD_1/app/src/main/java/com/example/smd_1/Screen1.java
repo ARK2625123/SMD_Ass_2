93b594d0dbba11bc60bc54e6e6cde7c13863705c
ARK2625123 <arkstar2625123@gmail.com>
2024-03-26 08:34:06 +0500

    Added back button on Screen 1 and made fragments on it

--- SMD_1/app/src/main/java/com/example/smd_1/Screen1.java
+++ SMD_1/app/src/main/java/com/example/smd_1/Screen1.java
@@ -4,20 +4,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Screen1 extends AppCompatActivity {
-
+ImageButton Bback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.screen_1);
+        init();
+        Bback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Screen1.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
 
+    }
+    public void init()
+    {
+        Bback=findViewById(R.id.Bback);
     }
 
 
